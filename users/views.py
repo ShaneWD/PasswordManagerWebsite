c207238ef156c0d86a09283ba72f900bf5d0bdac
Shane <>
2021-01-06 09:02:48 -0500

    modified views.py in users to render the form and template

--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .forms import UserRegisterForm
 
 # Create your views here.
+def register(request):
+    form = UserRegisterForm()
+    context = {
+        "form": form,
+    }
+    return render(request, "users/register.html", context)
