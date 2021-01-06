86072ca2d3bb899d867c319f1a4f370c2c790fb1
Shane <>
2021-01-06 09:40:44 -0500

    The email has to be unique to create an account (forms.py)

--- users/forms.py
+++ users/forms.py
@@ -1,13 +1,14 @@
 from django import forms 
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
     first_name = forms.CharField(required=True)
     last_name = forms.CharField(required=False)
 
-
+    model = User
     class Meta:
         model = User
         fields = [
@@ -18,3 +19,13 @@ class UserRegisterForm(UserCreationForm):
             'password1',
             'password2',
             ]
+    def save(self, commit=True):
+        user = super(UserRegisterForm, self).save(commit=False)
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.email = self.cleaned_data["email"]
+        if User.objects.filter(email=user.email).exists():
+            raise ValidationError("Email exists")
+        if commit:
+            user.save()
+        return user
--- users/views.py
+++ users/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import UserRegisterForm
 
 # Create your views here.
 def register(request):
-    form = UserRegisterForm()
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = UserRegisterForm()
     context = {
         "form": form,
     }
