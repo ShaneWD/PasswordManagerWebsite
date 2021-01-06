97ef7618de69e49e907be6fe57254c555a73b91d
Shane <>
2021-01-06 10:22:32 -0500

    /register/login/  is not functional; after logging in, it redirects user to home page

--- users/urls.py
+++ users/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from . import views
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register/', views.register, name="register"),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name="login" ),
 ]
