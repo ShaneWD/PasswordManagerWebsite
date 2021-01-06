831180c5d936567c8863ba9ded6328f997f6742d
Shane <>
2021-01-05 22:27:33 -0500

    created app 'main', modified urls to desplay message 'hello' as a test

--- main/urls.py
+++ main/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("main.urls")),
+    path('', views.home, name = "home"),
 ]
