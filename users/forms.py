7b78dfd07ab88a3326a3f4d2e48180793e56bc5c
Shane <>
2021-01-06 08:55:57 -0500

    created forms.py in users to for requiring the regular plus email, first & last name during registration

---
+++ users/forms.py
@@ -0,0 +1,20 @@
+from django import forms 
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=False)
+
+
+    class Meta:
+        model = User
+        fields = [
+            'username', 
+            'first_name',
+            'last_name',
+            'email',
+            'password1',
+            'password2',
+            ]
