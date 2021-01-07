0545f5c0985575fdfb25695d9c2cfddcee314855
Shane <>
2021-01-07 12:41:28 -0500

    added logic for save button. changed format of form

--- main/views.py
+++ main/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import LocationForm
 from .models import Location
 # Create your views here.
 def home(request):
-    form = LocationForm()
+    if request.method =="POST":
+        form = LocationForm(request.POST)
+        if form.is_valid:
+            form.save()
+            return redirect ("home")
+    else:
+        form = LocationForm()
     context = {
         "location":Location.objects.all(),
         "form": form
