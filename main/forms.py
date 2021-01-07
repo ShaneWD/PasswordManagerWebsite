5e92d094861a0f76c79809fbed85f278b07303d8
Shane <>
2021-01-07 11:25:31 -0500

    hope page shows form to fill out Location credentials

---
+++ main/forms.py
@@ -0,0 +1,14 @@
+from django import forms 
+
+from .models import Location
+
+class LocationForm(forms.ModelForm):
+    class Meta:
+        model = Location
+        fields = [
+            'website_name',
+            'website_link',
+            'website_username',
+            'website_password',
+            'website_notes',
+        ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import LocationForm
+from .models import Location
 # Create your views here.
 def home(request):
-    return render(request, "main/home.html")
+    form = LocationForm()
+    context = {
+        "location":Location.objects.all(),
+        "form": form
+    }
+    return render(request, "main/home.html", context)
