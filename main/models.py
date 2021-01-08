600c7bf9ba5c48993a61bedd9cb84d64cc433b8c
Shane <>
2021-01-07 23:42:50 -0500

    ❗ reseted &  reconfigured database; removed forign key in models ( in an attempt to solve issue #1 (Django Models: Null & Blank values)

--- main/models.py
+++ main/models.py
@@ -3,16 +3,14 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Location(models.Model):
-    main_user = models.ForeignKey(User, on_delete=models.CASCADE, default="DEFAULT VALUE")
-    
-    website_name = models.CharField(max_length=50, default='DEFAULT VALUE', unique=True)
+    website_name = models.CharField(max_length=50, unique=True)
     website_link = models.URLField(max_length=200, null=True)
-    website_username = models.CharField(max_length=50, default='DEFAULT VALUE')
-    website_password = models.CharField(max_length=50, default='DEFAULT VALUE')
+    website_username = models.CharField(max_length=50)
+    website_password = models.CharField(max_length=50)
     website_notes = models.CharField( max_length=50, null=True)
 
-    created = models.DateTimeField(auto_now=False, auto_now_add=True, null=True)
-    last_modified = models.DateTimeField(auto_now=True, auto_now_add=False, null=True)
+    created = models.DateTimeField( auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.website_name
--- main/views.py
+++ main/views.py
@@ -2,17 +2,19 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import LocationForm
 from .models import Location
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+@login_required
 def home(request):
     if request.method =="POST":
         form = LocationForm(request.POST)
-        if form.is_valid:
+        if form.is_valid():
             form.save()
             return redirect ("home")
     else:
         form = LocationForm()
     context = {
-        "location":Location.objects.all(),
+        # "location":Location.objects.all(),
         "form": form
     }
     return render(request, "main/home.html", context)
