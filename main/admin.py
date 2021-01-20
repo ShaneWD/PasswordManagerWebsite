0e0445dab1cee2a1c90b312645db7af0aaab880e
Shane <>
2021-01-20 09:20:35 -0500

    removed master password (which is always blank) & Location password from admin pannel

--- main/admin.py
+++ main/admin.py
@@ -6,5 +6,6 @@ admin.site.site_header = "ADMIN DASHBOARD: Password Manager"
 
 class AdminConfig(admin.ModelAdmin):
     list_display = ('website_name', 'author', 'created')
+    exclude = ('website_password', 'master_password',)
 
 admin.site.register(Location, AdminConfig)
