6b801bb0d32f4591ff90cefb669f9725391147da
Shane <>
2021-01-19 21:33:46 -0500

    admin pannel doesn't show (hashed) password for user

--- users/admin.py
+++ users/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from .models import User
 # Register your models here.
 
-admin.site.register(User)
+class AdminConfig(admin.ModelAdmin):
+    exclude = ('password',)
 
+admin.site.register(User, AdminConfig)
+
+ 
