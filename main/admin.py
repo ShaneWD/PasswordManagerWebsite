d0c03f2be3f0800bb14d07f7eddb886ee18d6acc
Shane <>
2021-01-19 21:24:41 -0500

    re-styled admin pannel

--- main/admin.py
+++ main/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from .models import Location
 # Register your models here.
 
-admin.site.register(Location)
+admin.site.site_header = "ADMIN DASHBOARD: Password Manager"
+
+class AdminConfig(admin.ModelAdmin):
+    list_display = ('website_name', 'author', 'created')
+
+admin.site.register(Location, AdminConfig)
