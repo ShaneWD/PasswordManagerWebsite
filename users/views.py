2912cdfef420a88217cc93543b5840f8627f20a2
Shane <>
2021-01-20 19:16:18 -0500

    styled the account (profile) page

--- users/views.py
+++ users/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
+@login_required
 def account(request):
+    
     context = {
         'user': request.user,
     }
+
     return render(request, "users/account.html", context)
 
 def register(request):
