6b7fafb6c21cfb3e35be49c6942301fe1dee840c
Shane <>
2021-01-25 13:04:47 -0500

    configured user display in admin pannel

--- users/admin.py
+++ users/admin.py
@@ -3,6 +3,7 @@ from .models import User
 # Register your models here.
 
 class AdminConfig(admin.ModelAdmin):
+    list_display = ('username', 'date_joined', 'is_staff', 'is_superuser')
     exclude = ('password',)
 
 admin.site.register(User, AdminConfig)
