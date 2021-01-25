20cbadea8358a54d1b04be95b5a753fc96890d3e
Shane <>
2021-01-25 13:52:42 -0500

    did minimum for setting up allauth

--- PWD/settings.py
+++ PWD/settings.py
@@ -36,8 +36,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'main.apps.MainConfig',
     'users.apps.UsersConfig',
+
+    'allauth',
+    'allauth.account',
 ]
 
 MIDDLEWARE = [
