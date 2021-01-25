d340e793eccfe3de746ea291fae8f81a0ac31061
Shane <>
2021-01-25 12:57:28 -0500

    key is now stored in environmental variable

--- PWD/settings.py
+++ PWD/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'r!bu(qe7zayy0&ixj!5qe7z(6+)u_bjp!dl2*==1+)8eq5leos'
-
+SECRET_KEY = os.environ.get('django_key_pwd_manager')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
