fdaaaa3cf5c12a61a2545cf228ee95b3f04ccf08
ShaneWD <DsiShaneW@gmail.com>
2021-08-25 11:13:22 -0400

    created a form for updating model Location

---
+++ main/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Location
+
+class LocationUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Location
+        fields = [
+            'website_name',
+            'website_link',
+            'website_username',
+            'website_password',
+            'website_notes',
+        ]
